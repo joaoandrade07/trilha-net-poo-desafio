63a693c2ec4e967415c50704ad3de3354d3a0663
João Andrade <joaodeandrade07@gmail.com>
2025-07-18 23:17:18 -0300

    :sparkles: feat: Metodos e propriedades adicionadas

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Nokia nokia = new("82912345678", "tijolão", "ab123", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Youtube");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Apple: ");
+Iphone iphone = new("82912345679", "XR", "ab125", 256);
+iphone.Ligar();
+iphone.InstalarAplicativo("Instagram");
